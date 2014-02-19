3aa940df1839be09ab6f5110c2833d6d4cbf75d7
hikaruna <hikaru.goldluna@gmail.com>
2014-02-20 00:39:05 +0900

    ModuleのメソッドがRListに依存していたのを修正

--- src/hikaruna/javaRubyLikeApi/module/RListModule.java
+++ src/hikaruna/javaRubyLikeApi/module/RListModule.java
@@ -3,9 +3,11 @@ package hikaruna.javaRubyLikeApi.module;
 import hikaruna.javaRubyLikeApi.Proc;
 import hikaruna.javaRubyLikeApi.RList;
 
+import java.util.List;
+
 public class RListModule {
 
-	public static <E> RList<E> select(RList<E> self, Proc<Boolean, E> proc) {
+	public static <E> RList<E> select(List<E> self, Proc<Boolean, E> proc) {
 		RList<E> ret = new RList<E>();
 		for(E item : self) {
 			if(proc.exec(item)) {

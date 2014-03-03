720ed3ecbcd460574bf5ae2de8f0c3e0f728ee2e
hikaruna <hikaru.goldluna@gmail.com>
2014-03-04 03:22:25 +0900

    sort関連のテストを追加 failed

--- src/hikaruna/javaRubyLikeApi/RList.java
+++ src/hikaruna/javaRubyLikeApi/RList.java
@@ -6,6 +6,7 @@ package hikaruna.javaRubyLikeApi;
 import hikaruna.javaRubyLikeApi.module.RListModule;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 /**
  * RubyのArray.
@@ -17,5 +18,24 @@ public class RList<E> extends ArrayList<E> {
 
 	public RList<E> select(Proc<Boolean, E> proc) {
 		return RListModule.select(this, proc);
+	}
+
+	public RList<E> sortBy() {
+		return null;
+	}
+
+	public RList<E> sortBy(Proc<?, E> proc) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public RList<E> sort() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public RList<E> sort(Comparator<E> comparator) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
 	}
 }
